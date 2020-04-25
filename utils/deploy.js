7bdb4a85dff22759a617ff0ff632d4067e80f435
Jonathan Zempel <jzempel@gmail.com>
2020-04-25 12:01:16 -0700

    Upgrade deploy script

---
+++ utils/deploy.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+const envalid = require('envalid');
+const garden = require('@zendeskgarden/scripts');
+const path = require('path');
+
+envalid.cleanEnv(process.env, {
+  GITHUB_TOKEN: envalid.str(),
+  NETLIFY_SITE_ID: envalid.str(),
+  NETLIFY_TOKEN: envalid.str()
+});
+
+(async () => {
+  const dir = path.resolve(__dirname, '..', 'public');
+  const branch = await garden.githubBranch();
+  const production = branch === 'master';
+  const repository = await garden.githubRepository();
+  const commit = await garden.githubCommit();
+  const message = `https://github.com/${repository.owner}/${repository.repo}/commit/${commit}`;
+  const command = async () => {
+    const result = await garden.netlifyDeploy({
+      dir,
+      production,
+      message
+    });
+
+    return result;
+  };
+
+  const url = await garden.githubDeploy({ command, production });
+
+  /* eslint-disable-next-line no-console */
+  console.log(`Deployed to ${url}`);
+})();
