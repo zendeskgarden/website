e09889cf40d82c37c516f3c95f2ebe073b860bf3
Ross Moody <fiftyfivehis@gmail.com>
2020-05-27 13:25:27 -0700

    anchor

---
+++ src/examples/anchor/AnchorExternal.tsx
@@ -0,0 +1,15 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { Anchor } from '@zendeskgarden/react-buttons';
+
+const Example = () => {
+  return <Anchor isExternal>Go to the Garden</Anchor>;
+};
+
+export default Example;
