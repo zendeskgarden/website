0004ed301e632f03dcd6e1b8f4b72414e1d00566
Ross Moody <fiftyfivehis@gmail.com>
2020-05-27 18:27:11 -0700

    breadcrumblies

---
+++ src/examples/breadcrumbs/BreadcrumbsDefault.tsx
@@ -0,0 +1,23 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { Anchor } from '@zendeskgarden/react-buttons';
+import { Span } from '@zendeskgarden/react-typography';
+import { Breadcrumb } from '@zendeskgarden/react-breadcrumbs';
+
+const Example = () => {
+  return (
+    <Breadcrumb>
+      <Anchor>Garden</Anchor>
+      <Anchor>Flowers</Anchor>
+      <Span>Roses</Span>
+    </Breadcrumb>
+  );
+};
+
+export default Example;
