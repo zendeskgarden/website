6ea167a2020810f0593dbd09f98df8bfe914305d
Ross Moody <fiftyfivehis@gmail.com>
2020-06-03 11:27:14 -0700

    lint fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,6 @@ export const SidebarPageFragment = graphql`
         version
         name
         description
-        peerDependencies
         packageName
       }
       propsSheets: reactPropSheets {
