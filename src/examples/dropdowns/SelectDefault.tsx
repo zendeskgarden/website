230b178c44300d43b166a24484d416e6617ea4f4
Henry Zhu <hz@henryzhu.me>
2020-06-05 15:24:34 -0700

    feat: add select component page (#52)

---
+++ src/examples/dropdowns/SelectDefault.tsx
@@ -0,0 +1,46 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React, { useState } from 'react';
+import { Dropdown, Field, Menu, Item, Select, Label } from '@zendeskgarden/react-dropdowns';
+
+interface IItem {
+  label: string;
+  value: string;
+}
+
+const items = [
+  { label: 'Cactus', value: 'item-1' },
+  { label: 'Orchid', value: 'item-2' },
+  { label: 'Succulent', value: 'item-3' }
+];
+
+const Example = () => {
+  const [selectedItem, setSelectedItem] = useState(items[0]);
+
+  return (
+    <Dropdown
+      selectedItem={selectedItem}
+      onSelect={setSelectedItem}
+      downshiftProps={{ itemToString: (item: IItem) => item && item.label }}
+    >
+      <Field>
+        <Label>Plant</Label>
+        <Select>{selectedItem.label}</Select>
+      </Field>
+      <Menu>
+        {items.map(option => (
+          <Item key={option.value} value={option}>
+            {option.label}
+          </Item>
+        ))}
+      </Menu>
+    </Dropdown>
+  );
+};
+
+export default Example;
