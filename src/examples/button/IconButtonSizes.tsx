f3ef7ed813f42110f902c40a94e82f11253f9899
Ross Moody <fiftyfivehis@gmail.com>
2020-06-22 14:51:16 -0700

    upload examples

---
+++ src/examples/button/IconButtonSizes.tsx
@@ -0,0 +1,29 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { Row } from '@zendeskgarden/react-grid';
+import { IconButton } from '@zendeskgarden/react-buttons';
+import { ReactComponent as ZendeskIcon } from '@zendeskgarden/svg-icons/src/26/zendesk.svg';
+
+const Example = () => {
+  return (
+    <Row justifyContent="around">
+      <IconButton size="small">
+        <ZendeskIcon />
+      </IconButton>
+      <IconButton>
+        <ZendeskIcon />
+      </IconButton>
+      <IconButton size="large">
+        <ZendeskIcon />
+      </IconButton>
+    </Row>
+  );
+};
+
+export default Example;
