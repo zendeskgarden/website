224457bde7e440da9b37ed7b262b0cdd07fa1684
hzhu <hz@henryzhu.me>
2020-06-23 11:01:45 -0700

    feat: add autocomplete component page

---
+++ src/examples/dropdowns/autocomplete/Media.tsx
@@ -0,0 +1,85 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React, { useRef, useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
+import { Item, Menu, Label, Field, Dropdown, Autocomplete } from '@zendeskgarden/react-dropdowns';
+import { Row, Col } from '@zendeskgarden/react-grid';
+import { ReactComponent as SearchIcon } from '@zendeskgarden/svg-icons/src/16/search-stroke.svg';
+
+const options = [
+  'Asparagus',
+  'Brussel sprouts',
+  'Cauliflower',
+  'Garlic',
+  'Jerusalem artichoke',
+  'Kale',
+  'Lettuce',
+  'Onion',
+  'Mushroom',
+  'Potato',
+  'Radish',
+  'Spinach',
+  'Tomato',
+  'Yam',
+  'Zucchini'
+];
+
+const Example = () => {
+  const [selectedItem, setSelectedItem] = useState(options[0]);
+  const [inputValue, setInputValue] = useState('');
+  const [matchingOptions, setMatchingOptions] = useState(options);
+
+  /**
+   * Debounce filtering
+   */
+  const filterMatchingOptionsRef = useRef(
+    debounce((value: string) => {
+      const matchedOptions = options.filter(
+        option => option.trim().toLowerCase().indexOf(value.trim().toLowerCase()) !== -1
+      );
+
+      setMatchingOptions(matchedOptions);
+    }, 300)
+  );
+
+  useEffect(() => {
+    filterMatchingOptionsRef.current(inputValue);
+  }, [inputValue]);
+
+  return (
+    <Row justifyContent="center">
+      <Col size="4">
+        <Dropdown
+          inputValue={inputValue}
+          selectedItem={selectedItem}
+          onSelect={item => setSelectedItem(item)}
+          onInputValueChange={value => setInputValue(value)}
+          downshiftProps={{ defaultHighlightedIndex: 0 }}
+        >
+          <Field>
+            <Label>Choose a vegetable</Label>
+            <Autocomplete start={<SearchIcon />}>{selectedItem}</Autocomplete>
+          </Field>
+          <Menu>
+            {matchingOptions.length ? (
+              matchingOptions.map(option => (
+                <Item key={option} value={option}>
+                  <span>{option}</span>
+                </Item>
+              ))
+            ) : (
+              <Item disabled>No matches found</Item>
+            )}
+          </Menu>
+        </Dropdown>
+      </Col>
+    </Row>
+  );
+};
+
+export default Example;
