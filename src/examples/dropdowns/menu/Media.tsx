c1688e5988bc1bb60982cfc7a64a0c4dff66f3e7
Jonathan Zempel <jzempel@gmail.com>
2020-06-26 09:18:40 -0700

    Update menus with chevron icons

--- src/examples/dropdowns/menu/Media.tsx
+++ src/examples/dropdowns/menu/Media.tsx
@@ -5,7 +5,7 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@zendeskgarden/react-buttons';
 import {
   Dropdown,
@@ -22,52 +22,65 @@ import {
   MediaFigure
 } from '@zendeskgarden/react-dropdowns';
 import { Row, Col } from '@zendeskgarden/react-grid';
+import { ReactComponent as ChevronIcon } from '@zendeskgarden/svg-icons/src/16/chevron-down-stroke.svg';
 import { ReactComponent as LeafIcon } from '@zendeskgarden/svg-icons/src/16/leaf-stroke.svg';
 
-const Example = () => (
-  <Row>
-    <Col textAlign="center">
-      <Dropdown
-        onSelect={item => {
-          const message =
-            item === 'add-plant' ? 'A plant can be added soon' : `You planted a ${item}`;
+const Example = () => {
+  const [rotated, setRotated] = useState<boolean | undefined>();
 
-          alert(message);
-        }}
-      >
-        <Trigger>
-          <Button>Menu</Button>
-        </Trigger>
-        <Menu>
-          <HeaderItem hasIcon>
-            <HeaderIcon>
-              <LeafIcon />
-            </HeaderIcon>
-            Plants
-          </HeaderItem>
-          <Separator />
-          <MediaItem value="cactus">
-            <MediaFigure>
-              <LeafIcon />
-            </MediaFigure>
-            <MediaBody>
-              Cactus
-              <ItemMeta>15 available</ItemMeta>
-            </MediaBody>
-          </MediaItem>
-          <Item value="flower">
-            <MediaFigure>
-              <LeafIcon />
-            </MediaFigure>
-            <MediaBody>Flower</MediaBody>
-          </Item>
-          <Item value="succulent">Succulent</Item>
-          <Separator />
-          <AddItem value="add-plant">Add plant</AddItem>
-        </Menu>
-      </Dropdown>
-    </Col>
-  </Row>
-);
+  return (
+    <Row>
+      <Col textAlign="center">
+        <Dropdown
+          onSelect={item => {
+            const message =
+              item === 'add-plant' ? 'A plant can be added soon' : `You planted a ${item}`;
+
+            alert(message);
+          }}
+          onStateChange={options =>
+            Object.prototype.hasOwnProperty.call(options, 'isOpen') && setRotated(options.isOpen)
+          }
+        >
+          <Trigger>
+            <Button>
+              Menu
+              <Button.EndIcon isRotated={rotated}>
+                <ChevronIcon />
+              </Button.EndIcon>
+            </Button>
+          </Trigger>
+          <Menu>
+            <HeaderItem hasIcon>
+              <HeaderIcon>
+                <LeafIcon />
+              </HeaderIcon>
+              Plants
+            </HeaderItem>
+            <Separator />
+            <MediaItem value="cactus">
+              <MediaFigure>
+                <LeafIcon />
+              </MediaFigure>
+              <MediaBody>
+                Cactus
+                <ItemMeta>15 available</ItemMeta>
+              </MediaBody>
+            </MediaItem>
+            <Item value="flower">
+              <MediaFigure>
+                <LeafIcon />
+              </MediaFigure>
+              <MediaBody>Flower</MediaBody>
+            </Item>
+            <Item value="succulent">Succulent</Item>
+            <Separator />
+            <AddItem value="add-plant">Add plant</AddItem>
+          </Menu>
+        </Dropdown>
+      </Col>
+    </Row>
+  );
+};
 
 export default Example;
