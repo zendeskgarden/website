29882207a9040c04f2682615bc34e1d01b90f1c2
Henry Zhu <hz@henryzhu.me>
2020-07-10 09:30:57 -0700

    feat: add typography component page (#100)

---
+++ src/examples/typography/Span.tsx
@@ -0,0 +1,36 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { Paragraph, Span, MD } from '@zendeskgarden/react-typography';
+
+const Example = () => (
+  <>
+    <Paragraph>
+      <MD tag="span">
+        Turnip greens yarrow ricebean rutabaga endive cauliflower sear lettuce kohlrabi amaranth
+        water spinach avacado daikon napa cabbage asparagus winter purslane kale. Celery potato
+        scallion desert raisin horseradish spinach carrot soko.
+      </MD>
+    </Paragraph>
+    <Paragraph>
+      <MD tag="span">
+        <Span isBold>
+          Lotus root spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin{' '}
+        </Span>
+        onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard
+        wakame kohlrabi beetroot carrot watercress.{' '}
+        <Span isMonospace>
+          <Span isBold>Cordn amaranth salsify bunya</Span> nuts nori azuki bean
+        </Span>
+        chickweed potato bell pepper artichoke.
+      </MD>
+    </Paragraph>
+  </>
+);
+
+export default Example;
