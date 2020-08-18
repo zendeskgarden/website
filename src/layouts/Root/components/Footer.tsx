a05f31533d33ba267a82a38c9c2e25ebb3554154
Ross Moody <fiftyfivehis@gmail.com>
2020-08-18 11:10:41 -0700

    Update Footer.tsx

--- src/layouts/Root/components/Footer.tsx
+++ src/layouts/Root/components/Footer.tsx
@@ -90,8 +90,7 @@ const Footer: React.FC = () => (
             padding: 0 ${p => p.theme.space.sm};
           `}
         >
-          The source of truth for tools, standards, and best practices when building products at
-          Zendesk
+          Garden is a design system by Zendesk
         </div>
         <div
           css={css`
