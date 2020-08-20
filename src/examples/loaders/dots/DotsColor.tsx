18b1796cafe7e7c6ac5c96a24076b8994f531672
Ross Moody <fiftyfivehis@gmail.com>
2020-08-20 11:22:35 -0700

    dots

---
+++ src/examples/loaders/dots/DotsColor.tsx
@@ -0,0 +1,27 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { Row, Col } from '@zendeskgarden/react-grid';
+import { PALETTE } from '@zendeskgarden/react-theming';
+import { Dots } from '@zendeskgarden/react-loaders';
+
+const Example = () => (
+  <Row alignItems="center">
+    <Col textAlign="center">
+      <Dots size={32} color={PALETTE.grey[600]} />
+    </Col>
+    <Col textAlign="center">
+      <Dots size={32} color={PALETTE.blue[600]} />
+    </Col>
+    <Col textAlign="center">
+      <Dots size={32} color={PALETTE.green[600]} />
+    </Col>
+  </Row>
+);
+
+export default Example;
