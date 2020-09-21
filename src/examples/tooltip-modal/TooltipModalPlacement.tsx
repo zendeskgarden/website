47d4733e9bbf80f9afd1e26f5579ca0b667dd538
Austin Green <agreen@zendesk.com>
2020-09-21 14:50:27 -0700

    Address JZ comments and change sidebar position

--- src/examples/tooltip-modal/TooltipModalPlacement.tsx
+++ src/examples/tooltip-modal/TooltipModalPlacement.tsx
@@ -9,12 +9,12 @@ import React, { useState, useRef, useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 import { TooltipModal } from '@zendeskgarden/react-modals';
 import { Row, Col } from '@zendeskgarden/react-grid';
-import { getColor } from '@zendeskgarden/react-theming';
 import { Button } from '@zendeskgarden/react-buttons';
+import { Span, SM } from '@zendeskgarden/react-typography';
 
 const StyledWrapperRow = styled(Row)`
   padding: ${p => p.theme.space.sm};
-  direction: ltr;
+  direction: ${p => (p.theme.rtl ? 'rtl' : 'ltr')};
   overflow: auto;
 `;
 
@@ -36,12 +36,6 @@ const StyledCenterRow = styled.div`
 
 const StyledCenterColumn = styled.div`
   min-width: 150px;
-`;
-
-const StyledProgress = styled(TooltipModal.FooterItem)`
-  flex-grow: 1;
-  color: ${p => getColor('neutralHue', 600, p.theme)};
-  font-size: ${p => p.theme.fontSizes.sm};
 `;
 
 const Example = () => {
@@ -87,134 +81,136 @@ const Example = () => {
   }, [placement]);
 
   return (
-    <StyledWrapperRow>
-      <StyledWrapperCol textAlign="center">
-        <StyledSpacedRow>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            top-start
-          </Button>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            top
-          </Button>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            top-end
-          </Button>
-        </StyledSpacedRow>
-        <StyledCenterRow>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            start-top
-          </Button>
-          <StyledCenterColumn></StyledCenterColumn>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            end-top
-          </Button>
-        </StyledCenterRow>
-        <StyledCenterRow>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            start
-          </Button>
-          <StyledCenterColumn>
-            <Button size="small" ref={storeRef} onClick={onClick}>
-              auto
-            </Button>
-          </StyledCenterColumn>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            end
-          </Button>
-        </StyledCenterRow>
-        <StyledCenterRow>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            start-bottom
-          </Button>
-          <StyledCenterColumn></StyledCenterColumn>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            end-bottom
-          </Button>
-        </StyledCenterRow>
-        <StyledSpacedRow>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            bottom-start
-          </Button>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            bottom
-          </Button>
-          <Button size="small" ref={storeRef} onClick={onClick}>
-            bottom-end
-          </Button>
-        </StyledSpacedRow>
-        <TooltipModal
-          referenceElement={referenceElement}
-          onClose={() => setReferenceElement(null)}
-          placement={placement as any}
-          popperModifiers={[
-            {
-              name: 'flip',
-              options: {
-                boundary: 'viewport'
-              }
-            },
-            {
-              name: 'preventOverflow',
-              options: {
-                boundary: 'viewport'
-              }
+    <>
+      <TooltipModal
+        referenceElement={referenceElement}
+        onClose={() => setReferenceElement(null)}
+        placement={placement as any}
+        popperModifiers={[
+          {
+            name: 'flip',
+            options: {
+              boundary: 'viewport'
             }
-          ]}
-        >
-          <TooltipModal.Title>Tooltip modal header</TooltipModal.Title>
-          <TooltipModal.Body>
-            Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea
-            sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber
-            earthnut pea peanut soko zucchini.
-          </TooltipModal.Body>
-          <TooltipModal.Footer>
-            <StyledProgress>
-              <span>{currentStep} of 13</span>
-            </StyledProgress>
-            <TooltipModal.FooterItem>
-              {currentStep > 1 && (
-                <Button
-                  size="small"
-                  isBasic
-                  onClick={() => {
-                    const placements = Object.keys(buttonRefs.current);
-                    const currentPlacementIndex = placements.indexOf(placement!);
-                    const previousPlacement = placements[currentPlacementIndex - 1];
-
-                    setPlacement(previousPlacement);
-                    setReferenceElement(buttonRefs.current[previousPlacement]);
-                  }}
-                >
-                  Previous
-                </Button>
-              )}
-            </TooltipModal.FooterItem>
-            <TooltipModal.FooterItem>
+          },
+          {
+            name: 'preventOverflow',
+            options: {
+              boundary: 'viewport'
+            }
+          }
+        ]}
+      >
+        <TooltipModal.Title>Tooltip modal header</TooltipModal.Title>
+        <TooltipModal.Body>
+          Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea
+          sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut
+          pea peanut soko zucchini.
+        </TooltipModal.Body>
+        <TooltipModal.Footer>
+          <SM style={{ flexGrow: 1 }}>
+            <Span hue="grey">{currentStep} of 13</Span>
+          </SM>
+          <TooltipModal.FooterItem>
+            {currentStep > 1 && (
               <Button
                 size="small"
-                isPrimary
+                isBasic
                 onClick={() => {
-                  if (currentStep === 13) {
-                    setPlacement(undefined);
-                    setReferenceElement(null);
-                  } else {
-                    const placements = Object.keys(buttonRefs.current);
-                    const currentPlacementIndex = placements.indexOf(placement!);
-                    const nextPlacement = placements[currentPlacementIndex + 1];
+                  const placements = Object.keys(buttonRefs.current);
+                  const currentPlacementIndex = placements.indexOf(placement!);
+                  const previousPlacement = placements[currentPlacementIndex - 1];
 
-                    setPlacement(nextPlacement);
-                    setReferenceElement(buttonRefs.current[nextPlacement]);
-                  }
+                  setPlacement(previousPlacement);
+                  setReferenceElement(buttonRefs.current[previousPlacement]);
                 }}
               >
-                {currentStep === 13 ? 'Finish' : 'Next'}
+                Previous
               </Button>
-            </TooltipModal.FooterItem>
-          </TooltipModal.Footer>
-          <TooltipModal.Close aria-label="Close" />
-        </TooltipModal>
-      </StyledWrapperCol>
-    </StyledWrapperRow>
+            )}
+          </TooltipModal.FooterItem>
+          <TooltipModal.FooterItem>
+            <Button
+              size="small"
+              isPrimary
+              onClick={() => {
+                if (currentStep === 13) {
+                  setPlacement(undefined);
+                  setReferenceElement(null);
+                } else {
+                  const placements = Object.keys(buttonRefs.current);
+                  const currentPlacementIndex = placements.indexOf(placement!);
+                  const nextPlacement = placements[currentPlacementIndex + 1];
+
+                  setPlacement(nextPlacement);
+                  setReferenceElement(buttonRefs.current[nextPlacement]);
+                }
+              }}
+            >
+              {currentStep === 13 ? 'Finish' : 'Next'}
+            </Button>
+          </TooltipModal.FooterItem>
+        </TooltipModal.Footer>
+        <TooltipModal.Close aria-label="Close" />
+      </TooltipModal>
+      <StyledWrapperRow>
+        <StyledWrapperCol textAlign="center">
+          <StyledSpacedRow>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              top-start
+            </Button>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              top
+            </Button>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              top-end
+            </Button>
+          </StyledSpacedRow>
+          <StyledCenterRow>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              start-top
+            </Button>
+            <StyledCenterColumn></StyledCenterColumn>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              end-top
+            </Button>
+          </StyledCenterRow>
+          <StyledCenterRow>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              start
+            </Button>
+            <StyledCenterColumn>
+              <Button size="small" ref={storeRef} onClick={onClick}>
+                auto
+              </Button>
+            </StyledCenterColumn>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              end
+            </Button>
+          </StyledCenterRow>
+          <StyledCenterRow>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              start-bottom
+            </Button>
+            <StyledCenterColumn></StyledCenterColumn>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              end-bottom
+            </Button>
+          </StyledCenterRow>
+          <StyledSpacedRow>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              bottom-start
+            </Button>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              bottom
+            </Button>
+            <Button size="small" ref={storeRef} onClick={onClick}>
+              bottom-end
+            </Button>
+          </StyledSpacedRow>
+        </StyledWrapperCol>
+      </StyledWrapperRow>
+    </>
   );
 };
 
