faabf4fa35a534750ffeb736a566a34dd32b203e
Ross Moody <fiftyfivehis@gmail.com>
2020-09-23 11:08:25 -0700

    feat(notifications): add notification, alert, well (#173)

---
+++ src/examples/notifications/well/WellMultiline.tsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { Well, Title, Paragraph } from '@zendeskgarden/react-notifications';
+
+const Example = () => (
+  <Well>
+    <Title>What is a Garden?</Title>
+    <Paragraph>
+      Turnip greens yarrow endive cauliflower sea lettuce kohlrabi amaranth water. Corn amaranth
+      salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.
+    </Paragraph>
+    <Paragraph>
+      Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic. Corn
+      amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke. Turnip
+      greens yarrow endive cauliflower sea lettuce kohlrabi amaranth water.
+    </Paragraph>
+  </Well>
+);
+
+export default Example;
