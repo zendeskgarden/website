3cd9a65c772c176beb6fe186664dbc3f630eac56
Austin Green <agreen@zendesk.com>
2020-10-22 13:10:44 -0700

    Address capitalization comments

--- src/examples/forms/input-group/Size.tsx
+++ src/examples/forms/input-group/Size.tsx
@@ -5,7 +5,7 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Row, Col } from '@zendeskgarden/react-grid';
 import { mediaQuery } from '@zendeskgarden/react-theming';
@@ -18,37 +18,29 @@ const StyledCol = styled(Col)`
   }
 `;
 
-const Example = () => {
-  const [value, setValue] = useState('Soil');
-
-  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  }, []);
-
-  return (
-    <Row justifyContent="center">
-      <Col sm={5}>
-        <Field>
-          <Label>Default</Label>
-          <InputGroup>
-            <Input value={value} onChange={onChange} />
-            <Button focusInset>Copy</Button>
-          </InputGroup>
-        </Field>
-      </Col>
-      <StyledCol sm={5}>
-        <Field>
-          <Label>Compact</Label>
-          <InputGroup isCompact>
-            <Input isCompact value={value} onChange={onChange} />
-            <Button size="small" focusInset>
-              Copy
-            </Button>
-          </InputGroup>
-        </Field>
-      </StyledCol>
-    </Row>
-  );
-};
+const Example = () => (
+  <Row justifyContent="center">
+    <Col sm={5}>
+      <Field>
+        <Label>Default</Label>
+        <InputGroup>
+          <Input />
+          <Button focusInset>Copy</Button>
+        </InputGroup>
+      </Field>
+    </Col>
+    <StyledCol sm={5}>
+      <Field>
+        <Label>Compact</Label>
+        <InputGroup isCompact>
+          <Input isCompact />
+          <Button size="small" focusInset>
+            Copy
+          </Button>
+        </InputGroup>
+      </Field>
+    </StyledCol>
+  </Row>
+);
 
 export default Example;
