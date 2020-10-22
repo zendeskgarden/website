a115f7ab0ddc388100d207c608bb509dee1b6e2f
Austin Green <agreen@zendesk.com>
2020-10-22 14:24:12 -0700

    feat: add Table drag-drop and virtualization examples

---
+++ src/examples/table/TableDraggable.tsx
@@ -0,0 +1,224 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React, { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import {
+  Table,
+  Caption,
+  Row,
+  Cell,
+  Head,
+  HeaderCell,
+  HeaderRow,
+  Body
+} from '@zendeskgarden/react-tables';
+import { ReactComponent as GripIcon } from '@zendeskgarden/svg-icons/src/12/grip.svg';
+
+const DraggableRow = styled(Row)<{ isDraggingOver: boolean }>`
+  ${props =>
+    props.isDraggingOver
+      ? `
+  :hover {
+    background-color: inherit !important;
+  }
+`
+      : ''}
+`;
+
+const StyledCaption = styled(Caption)`
+  margin-bottom: ${props => props.theme.space.sm};
+  line-height: ${props => props.theme.lineHeights.xl};
+  font-size: ${props => props.theme.fontSizes.xl};
+`;
+
+const DraggableContainer = styled.div`
+  :focus {
+    outline: none;
+  }
+`;
+
+interface IDraggableCellProps {
+  isDragOccurring?: boolean;
+}
+
+class DraggableCell extends React.Component<IDraggableCellProps> {
+  ref: HTMLTableCellElement | null = null;
+
+  constructor(args: any) {
+    super(args);
+
+    this.setRef = this.setRef.bind(this);
+  }
+
+  getSnapshotBeforeUpdate(prevProps: IDraggableCellProps) {
+    if (!this.ref) {
+      return null;
+    }
+
+    const isDragStarting = this.props.isDragOccurring && !prevProps.isDragOccurring;
+
+    if (!isDragStarting) {
+      return null;
+    }
+
+    const { width, height } = this.ref.getBoundingClientRect();
+
+    const snapshot = {
+      width,
+      height
+    };
+
+    return snapshot;
+  }
+
+  componentDidUpdate(prevProps: IDraggableCellProps, prevState: any, snapshot: any) {
+    const ref = this.ref;
+
+    if (!ref) {
+      return;
+    }
+
+    if (snapshot) {
+      if (ref.style.width === snapshot.width) {
+        return;
+      }
+
+      ref.style.width = `${snapshot.width}px`;
+      ref.style.height = `${snapshot.height}px`;
+
+      return;
+    }
+
+    if (this.props.isDragOccurring) {
+      return;
+    }
+
+    if (ref.style.width === null) {
+      return;
+    }
+
+    ref.style.removeProperty('height');
+    ref.style.removeProperty('width');
+  }
+
+  setRef(ref: HTMLTableCellElement | null) {
+    this.ref = ref;
+  }
+
+  render() {
+    return <Cell ref={this.setRef}>{this.props.children}</Cell>;
+  }
+}
+
+interface IItem {
+  id: string;
+  content: string;
+}
+
+const generateItems = (count: number) =>
+  Array.from({ length: count }, (v, k) => k).map<IItem>(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+const reorderItems = (list: IItem[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const Example = () => {
+  const [items, setItems] = useState(generateItems(10));
+  const [recentDragId, setRecentDragId] = useState<string>();
+
+  useEffect(() => {
+    if (recentDragId) {
+      const draggable = document.getElementById(recentDragId);
+
+      draggable && draggable.focus();
+    }
+  }, [recentDragId]);
+
+  const onDragEnd = useCallback(
+    (result: DropResult) => {
+      if (!result.destination) {
+        return;
+      }
+
+      const newItems = reorderItems(items, result.source.index, result.destination.index);
+
+      setItems(newItems);
+      setRecentDragId(result.draggableId);
+    },
+    [items]
+  );
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Table>
+        <StyledCaption>Your Unsolved Tickets</StyledCaption>
+        <Head>
+          <HeaderRow>
+            <HeaderCell isMinimum />
+            <HeaderCell>Subject</HeaderCell>
+            <HeaderCell>Requester</HeaderCell>
+            <HeaderCell>Requested</HeaderCell>
+            <HeaderCell>Type</HeaderCell>
+          </HeaderRow>
+        </Head>
+        <Droppable droppableId="droppable">
+          {(droppableProps, droppableSnapshot) => {
+            return (
+              <Body ref={droppableProps.innerRef}>
+                {items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <DraggableRow
+                        ref={provided.innerRef}
+                        isDraggingOver={droppableSnapshot.isDraggingOver}
+                        isHovered={snapshot.isDragging}
+                        isFocused={
+                          droppableSnapshot.isDraggingOver ? snapshot.isDragging : undefined
+                        }
+                        {...provided.draggableProps.style}
+                        {...provided.draggableProps}
+                      >
+                        <DraggableCell isDragOccurring={snapshot.isDragging}>
+                          <DraggableContainer id={item.id} {...provided.dragHandleProps}>
+                            <GripIcon />
+                          </DraggableContainer>
+                        </DraggableCell>
+                        <DraggableCell isDragOccurring={snapshot.isDragging}>
+                          {item.content}
+                        </DraggableCell>
+                        <DraggableCell isDragOccurring={snapshot.isDragging}>
+                          John Smith
+                        </DraggableCell>
+                        <DraggableCell isDragOccurring={snapshot.isDragging}>
+                          15 minutes ago
+                        </DraggableCell>
+                        <DraggableCell isDragOccurring={snapshot.isDragging}>Ticket</DraggableCell>
+                      </DraggableRow>
+                    )}
+                  </Draggable>
+                ))}
+                {droppableProps.placeholder}
+              </Body>
+            );
+          }}
+        </Droppable>
+      </Table>
+    </DragDropContext>
+  );
+};
+
+export default Example;
---
+++ src/examples/table/TableVirtualized.tsx
@@ -0,0 +1,94 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import { FixedSizeList } from 'react-window';
+import { Table, Head, HeaderRow, HeaderCell, Body, Row, Cell } from '@zendeskgarden/react-tables';
+
+const rowData: {
+  id: number;
+  subject: string;
+  requester: string;
+  requested: string;
+  type: string;
+}[] = [];
+
+for (let x = 1; x <= 100000; x++) {
+  rowData.push({
+    id: x,
+    subject: 'Example subject',
+    requester: 'John Doe',
+    requested: '15 minutes ago',
+    type: 'Ticket'
+  });
+}
+
+/* stylelint-disable declaration-no-important */
+const ScrollableTable = styled(Table).attrs({ role: 'presentation' })`
+  display: block !important;
+`;
+
+const ScrollableHead = styled(Head)`
+  display: block;
+`;
+
+const ScrollableHeaderRow = styled(HeaderRow).attrs({ role: 'row' })`
+  display: table !important;
+  width: 100%;
+  table-layout: fixed;
+`;
+
+const ScrollableHeaderCell = styled(HeaderCell).attrs({ role: 'columnheader' })``;
+
+const ScrollableBody = styled(Body)`
+  display: block !important;
+`;
+
+const ScrollableRow = styled(Row).attrs({ role: 'row' })`
+  display: table !important;
+  table-layout: fixed;
+`;
+
+const ScrollableCell = styled(Cell).attrs({ role: 'cell' })``;
+/* stylelint-enable declaration-no-important */
+
+const Example = () => (
+  <div role="grid" aria-rowcount={rowData.length} aria-colcount={4}>
+    <ScrollableTable>
+      <ScrollableHead>
+        <ScrollableHeaderRow>
+          <ScrollableHeaderCell>Subject</ScrollableHeaderCell>
+          <ScrollableHeaderCell>Requester</ScrollableHeaderCell>
+          <ScrollableHeaderCell>Requested</ScrollableHeaderCell>
+          <ScrollableHeaderCell>Type</ScrollableHeaderCell>
+        </ScrollableHeaderRow>
+      </ScrollableHead>
+    </ScrollableTable>
+    <FixedSizeList
+      height={500}
+      itemCount={rowData.length}
+      itemSize={40}
+      width="100%"
+      outerElementType={ScrollableTable}
+      innerElementType={ScrollableBody}
+    >
+      {({ index, style }) => (
+        <ScrollableRow key={rowData[index].id} style={style} aria-rowindex={index + 1}>
+          <ScrollableCell isTruncated>
+            [{rowData[index].id}] {rowData[index].subject}
+          </ScrollableCell>
+          <ScrollableCell isTruncated>{rowData[index].requester}</ScrollableCell>
+          <ScrollableCell isTruncated>{rowData[index].requested}</ScrollableCell>
+          <ScrollableCell isTruncated>{rowData[index].type}</ScrollableCell>
+        </ScrollableRow>
+      )}
+    </FixedSizeList>
+  </div>
+);
+
+export default Example;
