49d1bc55e0912a30dc53ecb5fd3f68924ecab77b
Jonathan Zempel <jzempel@gmail.com>
2020-11-17 15:21:04 -0800

    Use updated Blockquote/Paragraph

--- src/examples/typography/Blockquote.tsx
+++ src/examples/typography/Blockquote.tsx
@@ -6,16 +6,7 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
 import { Blockquote, Paragraph, MD } from '@zendeskgarden/react-typography';
-
-const StyledBlockquote = styled(Blockquote)`
-  margin-top: ${p => p.theme.space.md};
-`;
-
-const StyledParagraph = styled(Paragraph)`
-  margin-top: ${p => p.theme.space.md};
-`;
 
 const Example = () => (
   <MD>
@@ -23,18 +14,18 @@ const Example = () => (
       Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth
       tatsoi tomatillo melon azuki bean garlic.
     </Paragraph>
-    <StyledBlockquote>Life begins the day you start a garden.</StyledBlockquote>
-    <StyledParagraph>
+    <Blockquote>Life begins the day you start a garden.</Blockquote>
+    <Paragraph>
       Lotus root spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin
       onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard
       wakame kohlrabi beetroot carrot watercress. Cordn amaranth salsify bunya nuts nori azuki bean
       chickweed potato bell pepper artichoke.
-    </StyledParagraph>
-    <StyledBlockquote>
+    </Paragraph>
+    <Blockquote>
       In garden arrangement, as in all other kinds of decorative work, one has not only to acquire a
       knowledge of what to do, but also to gain some wisdom in perceiving what it is well to let
       alone.
-    </StyledBlockquote>
+    </Blockquote>
   </MD>
 );
 
