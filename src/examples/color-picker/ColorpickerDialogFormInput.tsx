4dc26108bbbf895bcdc211ff74290034a329c8e7
hzhu <hz@henryzhu.me>
2021-03-30 16:03:18 -0700

    Fix typing for rgba function

--- src/examples/color-picker/ColorpickerDialogFormInput.tsx
+++ src/examples/color-picker/ColorpickerDialogFormInput.tsx
@@ -13,6 +13,13 @@ import { ColorpickerDialog, IColor } from '@zendeskgarden/react-colorpickers';
 import { Field, Label, Input, InputGroup } from '@zendeskgarden/react-forms';
 import { DEFAULT_THEME } from '@zendeskgarden/react-theming';
 
+interface IRgbaColor {
+  red: number;
+  green: number;
+  blue: number;
+  alpha: number;
+}
+
 const validHex = /^#(?:(?:[0-9A-F]{6}(?:[0-9A-F]{2})?)|(?:[0-9A-F]{3})(?:[0-9A-F]?))$/iu;
 
 const StyledCol = styled(Col)`
@@ -51,7 +58,7 @@ const toHex = (selectedColor: IColor) => {
 const Example = () => {
   const defaultColor = `${DEFAULT_THEME.palette.blue[600]}99`;
   const [input, setInput] = useState(defaultColor);
-  const [color, setColor] = useState<string | IColor>(rgba(parseToRgb(defaultColor) as RgbColor));
+  const [color, setColor] = useState<string | IColor>(rgba(parseToRgb(defaultColor) as IRgbaColor));
 
   return (
     <Row justifyContent="center">
