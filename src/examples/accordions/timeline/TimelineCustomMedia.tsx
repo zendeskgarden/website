cee8743acd708cf7da6b613feaafda9b5ae6cd8d
Smriti Madan <smriti.madan@zendesk.com>
2021-09-13 17:36:04 -0400

    Replace Zendesk avatar style icon with garden icons in custom media example

--- src/examples/accordions/timeline/TimelineCustomMedia.tsx
+++ src/examples/accordions/timeline/TimelineCustomMedia.tsx
@@ -6,24 +6,19 @@
  */
 
 import React from 'react';
-import { PALETTE } from '@zendeskgarden/react-theming';
 import { Timeline } from '@zendeskgarden/react-accordions';
 import { Row, Col } from '@zendeskgarden/react-grid';
 import { Span } from '@zendeskgarden/react-typography';
-import { Avatar } from '@zendeskgarden/react-avatars';
-import { ReactComponent as ZendeskIcon } from '@zendeskgarden/svg-icons/src/26/zendesk.svg';
-
-const zendeskAvatar = (
-  <Avatar backgroundColor={PALETTE.kale[700]} size="extrasmall" isSystem>
-    <ZendeskIcon role="img" aria-label="Zendesk" />
-  </Avatar>
-);
+import { ReactComponent as EyeIcon } from '@zendeskgarden/svg-icons/src/12/eye-stroke.svg';
+import { ReactComponent as EmailIcon } from '@zendeskgarden/svg-icons/src/12/email-stroke.svg';
+import { ReactComponent as CartIcon } from '@zendeskgarden/svg-icons/src/12/shopping-cart-stroke.svg';
+import { ReactComponent as ClipboardIcon } from '@zendeskgarden/svg-icons/src/12/clipboard-blank-stroke.svg';
 
 const Example = () => (
   <Row justifyContent="center">
     <Col sm={4}>
       <Timeline>
-        <Timeline.Item icon={zendeskAvatar}>
+        <Timeline.Item icon={<EmailIcon />}>
           <Timeline.Content>
             <Span isBold style={{ display: 'block' }}>
               Issue with order
@@ -31,7 +26,7 @@ const Example = () => (
             <Span hue="grey">Today 9:00 AM</Span>
           </Timeline.Content>
         </Timeline.Item>
-        <Timeline.Item icon={zendeskAvatar}>
+        <Timeline.Item icon={<ClipboardIcon />}>
           <Timeline.Content>
             <Span isBold style={{ display: 'block' }}>
               Ordered 3 items
@@ -39,7 +34,7 @@ const Example = () => (
             <Span hue="grey">Feb 08, 9:05 AM</Span>
           </Timeline.Content>
         </Timeline.Item>
-        <Timeline.Item icon={zendeskAvatar}>
+        <Timeline.Item icon={<CartIcon />}>
           <Timeline.Content>
             <Span isBold style={{ display: 'block' }}>
               Added 3 items to cart
@@ -47,7 +42,7 @@ const Example = () => (
             <Span hue="grey">Jan 21, 9:13 AM</Span>
           </Timeline.Content>
         </Timeline.Item>
-        <Timeline.Item icon={zendeskAvatar}>
+        <Timeline.Item icon={<EyeIcon />}>
           <Timeline.Content>
             <Span isBold style={{ display: 'block' }}>
               Viewed product page
