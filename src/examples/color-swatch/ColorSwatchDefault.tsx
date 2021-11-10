b3b2173867183bdebdbd6c7d21856fb28161ed97
hzhu <hz@henryzhu.me>
2021-11-10 11:26:49 -0800

    Refactor to use palette

--- src/examples/color-swatch/ColorSwatchDefault.tsx
+++ src/examples/color-swatch/ColorSwatchDefault.tsx
@@ -9,39 +9,31 @@ import React from 'react';
 import { ColorSwatch } from '@zendeskgarden/react-colorpickers';
 import { Row, Col } from '@zendeskgarden/react-grid';
 import { convertToMatrix } from '@zendeskgarden/container-utilities';
+import { PALETTE } from '@zendeskgarden/react-theming';
 
-const colors = [
-  { label: 'Blue-200', value: '#cee2f2' },
-  { label: 'Blue-300', value: '#adcce4' },
-  { label: 'Blue-400', value: '#5293c7' },
-  { label: 'Blue-500', value: '#337fbd' },
-  { label: 'Blue-600', value: '#1f73b7' },
-  { label: 'Blue-700', value: '#144a75' },
-  { label: 'Blue-800', value: '#0f3554' },
-  { label: 'Red-200', value: '#f5d5d8' },
-  { label: 'Red-300', value: '#f5b5ba' },
-  { label: 'Red-400', value: '#e35b66' },
-  { label: 'Red-500', value: '#d93f4c' },
-  { label: 'Red-600', value: '#cc3340' },
-  { label: 'Red-700', value: '#8c232c' },
-  { label: 'Red-800', value: '#681219' },
-  { label: 'Yellow-200', value: '#ffeedb' },
-  { label: 'Yellow-300', value: '#fed6a8' },
-  { label: 'Yellow-400', value: '#ffb057' },
-  { label: 'Yellow-500', value: '#f79a3e' },
-  { label: 'Yellow-600', value: '#ed8f1c' },
-  { label: 'Yellow-700', value: '#ad5918' },
-  { label: 'Yellow-800', value: '#703815' },
-  { label: 'Green-200', value: '#d1e8df' },
-  { label: 'Green-300', value: '#aecfc2' },
-  { label: 'Green-400', value: '#5eae91' },
-  { label: 'Green-500', value: '#228f67' },
-  { label: 'Green-600', value: '#038153' },
-  { label: 'Green-700', value: '#186146' },
-  { label: 'Green-800', value: '#0b3b29' }
-];
+interface ILabledColor {
+  label: string;
+  value: string;
+}
 
-const matrix = convertToMatrix(colors, 7);
+const colors = ['blue', 'red', 'yellow', 'green'];
+
+const shades = [200, 300, 400, 500, 600, 700, 800];
+
+const capitalize = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const labeledColors = colors.reduce(
+  (acc: ILabledColor[], hue: string) => [
+    ...acc,
+    ...shades.map(shade => ({
+      label: `${capitalize(hue)}-${shade}`,
+      value: (PALETTE as any)[hue][shade]
+    }))
+  ],
+  []
+);
+
+const matrix = convertToMatrix(labeledColors, 7);
 
 const Example = () => (
   <Row justifyContent="center">
