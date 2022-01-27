74823ae279c1545d8986cf5786b9d978bfabaa2d
Mike Tobia <Francois-Esquire@users.noreply.github.com>
2022-01-27 16:17:58 -0500

    feat(combobox): add documentation page for component (#324)

--- src/components/MarkdownProvider/components/BestPractice.tsx
+++ src/components/MarkdownProvider/components/BestPractice.tsx
@@ -118,6 +118,7 @@ interface ISectionProps extends ICaptionProps {
   imageHeight?: number;
   imageWidth?: number;
   imageIsSquare?: boolean;
+  imageIsFreeForm?: boolean;
 }
 
 export const Section: React.FC<ISectionProps> = props => {
@@ -126,7 +127,7 @@ export const Section: React.FC<ISectionProps> = props => {
       width: props.imageWidth,
       height: props.imageHeight,
       maxWidth: props.imageIsSquare ? 160 : undefined,
-      maxHeight: 160
+      maxHeight: props.imageIsFreeForm ? undefined : 160
     };
 
     return (
@@ -185,10 +186,21 @@ export const Caution: React.FC<ISectionProps> = props => (
   />
 );
 
-export const BestPractice: React.FC = props => (
-  <StyledRow>
-    {React.Children.map(props.children, child => (
-      <StyledCol sm>{child}</StyledCol>
-    ))}
-  </StyledRow>
-);
+interface IBestPracticesProps {
+  rows?: boolean;
+}
+
+export const BestPractice: React.FC<IBestPracticesProps> = ({ rows, ...props }) =>
+  rows ? (
+    <>
+      {React.Children.map(props.children, child => (
+        <StyledRow>{child}</StyledRow>
+      ))}
+    </>
+  ) : (
+    <StyledRow>
+      {React.Children.map(props.children, child => (
+        <StyledCol sm>{child}</StyledCol>
+      ))}
+    </StyledRow>
+  );
---
+++ src/examples/dropdowns/combobox/Hidden.tsx
@@ -0,0 +1,97 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React, { useMemo, useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
+import { Item, Menu, Label, Field, Dropdown, Combobox } from '@zendeskgarden/react-dropdowns';
+import { Row, Col } from '@zendeskgarden/react-grid';
+import { ReactComponent as SearchIcon } from '@zendeskgarden/svg-icons/src/16/search-stroke.svg';
+
+const options = [
+  'Asparagus',
+  'Brussel sprouts',
+  'Cauliflower',
+  'Garlic',
+  'Jerusalem artichoke',
+  'Kale',
+  'Lettuce',
+  'Onion',
+  'Mushroom',
+  'Potato',
+  'Radish',
+  'Spinach',
+  'Tomato',
+  'Yam',
+  'Zucchini'
+];
+
+const Example = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [matchingOptions, setMatchingOptions] = useState<string[]>([]);
+
+  /**
+   * Debounce filtering
+   */
+  const filterMatchingOptions = useMemo(
+    () =>
+      debounce((value: string) => {
+        if (value.length > 0) {
+          const valueRegexp = new RegExp(value!, 'gui');
+
+          setMatchingOptions(options.filter(option => option.match(valueRegexp)) || []);
+        } else {
+          setMatchingOptions([]);
+        }
+        setIsLoading(false);
+      }, 250),
+    []
+  );
+
+  useEffect(() => {
+    setIsLoading(true);
+    filterMatchingOptions(inputValue);
+
+    return () => filterMatchingOptions.cancel();
+  }, [filterMatchingOptions, inputValue]);
+
+  const renderOptions = () => {
+    if (isLoading) {
+      return <Item disabled>Loading items...</Item>;
+    }
+
+    if (matchingOptions.length === 0) {
+      return <Item disabled>No matches found</Item>;
+    }
+
+    return matchingOptions.map(option => (
+      <Item key={option} value={option}>
+        <span>{option}</span>
+      </Item>
+    ));
+  };
+
+  return (
+    <Row justifyContent="center">
+      <Col sm={5}>
+        <Dropdown
+          inputValue={inputValue}
+          onSelect={item => setInputValue(item)}
+          onInputValueChange={value => setInputValue(value)}
+        >
+          <Field>
+            <Label hidden>Accessibly hidden label</Label>
+            <Combobox start={<SearchIcon />} placeholder="Search vegetables" />
+          </Field>
+          <Menu>{renderOptions()}</Menu>
+        </Dropdown>
+      </Col>
+    </Row>
+  );
+};
+
+export default Example;
