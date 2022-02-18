cbcc3a075342a4dd5f1cec34024949474683f952
Deric Kwok <derickwok25@gmail.com>
2022-02-18 12:33:04 -0800

    refactor grid usage for icon

--- src/examples/design/icons/IconShowcase.tsx
+++ src/examples/design/icons/IconShowcase.tsx
@@ -6,36 +6,38 @@
  */
 
 import React from 'react';
-import { Grid, Row, Col } from '@zendeskgarden/react-grid';
-import { DEFAULT_THEME } from '@zendeskgarden/react-theming';
+import styled from 'styled-components';
+import { Row, Col } from '@zendeskgarden/react-grid';
+import { getColor } from '@zendeskgarden/react-theming';
 import { ReactComponent as LeafIcon } from '@zendeskgarden/svg-icons/src/16/leaf-stroke.svg';
 import { ReactComponent as LightBulbIcon } from '@zendeskgarden/svg-icons/src/16/lightbulb-stroke.svg';
 import { ReactComponent as LightningBoltIcon } from '@zendeskgarden/svg-icons/src/16/lightning-bolt-stroke.svg';
 import { ReactComponent as LocationIcon } from '@zendeskgarden/svg-icons/src/16/location-stroke.svg';
 import { ReactComponent as SmileyIcon } from '@zendeskgarden/svg-icons/src/16/smiley-stroke.svg';
 
-const ICON_FILL = DEFAULT_THEME.palette.grey[600];
+const StyledCol = styled(Col)`
+  text-align: center;
+  color: ${p => getColor('neutralHue', 600, p.theme)};
+`;
 
 const Example = () => (
-  <Grid>
-    <Row alignItems="center" justifyContent="center" style={{ minHeight: '5em' }}>
-      <Col size={1}>
-        <LeafIcon color={ICON_FILL} style={{ width: '100%', margin: 'auto' }} />
-      </Col>
-      <Col size={1}>
-        <LightBulbIcon color={ICON_FILL} style={{ width: '100%', margin: 'auto' }} />
-      </Col>
-      <Col size={1}>
-        <LightningBoltIcon color={ICON_FILL} style={{ width: '100%', margin: 'auto' }} />
-      </Col>
-      <Col size={1}>
-        <LocationIcon color={ICON_FILL} style={{ width: '100%', margin: 'auto' }} />
-      </Col>
-      <Col size={1}>
-        <SmileyIcon color={ICON_FILL} style={{ width: '100%', margin: 'auto' }} />
-      </Col>
-    </Row>
-  </Grid>
+  <Row alignItems="center" justifyContent="center" style={{ minHeight: '5em' }}>
+    <StyledCol xs md={1}>
+      <LeafIcon />
+    </StyledCol>
+    <StyledCol xs md={1}>
+      <LightBulbIcon />
+    </StyledCol>
+    <StyledCol xs md={1}>
+      <LightningBoltIcon />
+    </StyledCol>
+    <StyledCol xs md={1}>
+      <LocationIcon />
+    </StyledCol>
+    <StyledCol xs md={1}>
+      <SmileyIcon />
+    </StyledCol>
+  </Row>
 );
 
 export default Example;
--- src/examples/design/icons/IconStyles.tsx
+++ src/examples/design/icons/IconStyles.tsx
@@ -8,30 +8,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import { MD } from '@zendeskgarden/react-typography';
-import { Grid, Row, Col } from '@zendeskgarden/react-grid';
+import { Row, Col } from '@zendeskgarden/react-grid';
 import { ReactComponent as LeafStrokeIcon } from '@zendeskgarden/svg-icons/src/16/leaf-stroke.svg';
 import { ReactComponent as LeafFillIcon } from '@zendeskgarden/svg-icons/src/16/leaf-fill.svg';
 import { getColor } from '@zendeskgarden/react-theming';
 
 const StyledCol = styled(Col)`
   text-align: center;
-  color: ${p => getColor('gray', 600, p.theme)};
+  color: ${p => getColor('neutralHue', 600, p.theme)};
 `;
 
 const Example = () => {
   return (
-    <Grid>
-      <Row justifyContent="center">
-        <StyledCol size={3}>
-          <LeafStrokeIcon />
-          <MD>Stroke style (default)</MD>
-        </StyledCol>
-        <StyledCol size={3}>
-          <LeafFillIcon />
-          <MD>Filled Styled</MD>
-        </StyledCol>
-      </Row>
-    </Grid>
+    <Row justifyContent="center">
+      <StyledCol xs md={3}>
+        <LeafStrokeIcon />
+        <MD>Stroke style (default)</MD>
+      </StyledCol>
+      <StyledCol xs md={3}>
+        <LeafFillIcon />
+        <MD>Filled Styled</MD>
+      </StyledCol>
+    </Row>
   );
 };
 
