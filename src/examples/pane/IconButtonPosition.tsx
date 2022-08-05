27aff6b32d3fdb88b5ead08650b7f09406f90ec8
Smriti Madan <smriti.madan@zendesk.com>
2022-08-05 18:32:09 -0400

    Add headings to Splitter button positions example

--- src/examples/pane/IconButtonPosition.tsx
+++ src/examples/pane/IconButtonPosition.tsx
@@ -8,12 +8,17 @@
 import React from 'react';
 import { PaneProvider, Pane } from '@zendeskgarden/react-grid';
 import useResizeObserver from 'use-resize-observer';
-import { XL, Paragraph } from '@zendeskgarden/react-typography';
+import { XL, XXL, Paragraph } from '@zendeskgarden/react-typography';
 import styled from 'styled-components';
 import { getColor } from '@zendeskgarden/react-theming';
 
 const StyledDiv = styled.div`
   padding: ${p => p.theme.space.base * 6}px;
+`;
+
+const StyledXXL = styled(XXL)`
+  margin-bottom: ${p => p.theme.space.base * 6}px;
+  text-transform: capitalize;
 `;
 
 const StyledParagraph = styled(Paragraph)`
@@ -43,52 +48,55 @@ const Example = () => {
   return (
     <div ref={ref}>
       {iconPositions.map(position => (
-        <PaneProvider
-          totalPanesHeight={height}
-          totalPanesWidth={width}
-          defaultColumnValues={{
-            'column-1': 10,
-            'column-2': 10
-          }}
-          key={`${position}-provider`}
-        >
-          {({ getGridTemplateColumns }) => (
-            <StyledPanesContainer
-              style={{
-                width,
-                height: '100%',
-                display: 'grid',
-                gridTemplateColumns: getGridTemplateColumns()
-              }}
-            >
-              <Pane>
-                <StyledDiv>
-                  <StyledPaneContent>
-                    <XL>Pane 1</XL>
-                    <StyledParagraph>
-                      Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi
-                      amaranth water spinach avocado.
-                    </StyledParagraph>
-                  </StyledPaneContent>
-                </StyledDiv>
-                <Pane.Splitter layoutKey="column-1" min={5} max={10} aria-label="pane1 splitter">
-                  <Pane.SplitterButton label="Toggle column-1" placement={position} />
-                </Pane.Splitter>
-              </Pane>
-              <Pane>
-                <StyledDiv>
-                  <StyledPaneContent>
-                    <XL>Pane 2</XL>
-                    <StyledParagraph>
-                      Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi
-                      amaranth water spinach avocado.
-                    </StyledParagraph>
-                  </StyledPaneContent>
-                </StyledDiv>
-              </Pane>
-            </StyledPanesContainer>
-          )}
-        </PaneProvider>
+        <>
+          <StyledXXL isBold>{position}</StyledXXL>
+          <PaneProvider
+            totalPanesHeight={height}
+            totalPanesWidth={width}
+            defaultColumnValues={{
+              'column-1': 10,
+              'column-2': 10
+            }}
+            key={`${position}-provider`}
+          >
+            {({ getGridTemplateColumns }) => (
+              <StyledPanesContainer
+                style={{
+                  width,
+                  height: '100%',
+                  display: 'grid',
+                  gridTemplateColumns: getGridTemplateColumns()
+                }}
+              >
+                <Pane>
+                  <StyledDiv>
+                    <StyledPaneContent>
+                      <XL>Pane 1</XL>
+                      <StyledParagraph>
+                        Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce
+                        kohlrabi amaranth water spinach avocado.
+                      </StyledParagraph>
+                    </StyledPaneContent>
+                  </StyledDiv>
+                  <Pane.Splitter layoutKey="column-1" min={5} max={10} aria-label="pane1 splitter">
+                    <Pane.SplitterButton label="Toggle column-1" placement={position} />
+                  </Pane.Splitter>
+                </Pane>
+                <Pane>
+                  <StyledDiv>
+                    <StyledPaneContent>
+                      <XL>Pane 2</XL>
+                      <StyledParagraph>
+                        Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce
+                        kohlrabi amaranth water spinach avocado.
+                      </StyledParagraph>
+                    </StyledPaneContent>
+                  </StyledDiv>
+                </Pane>
+              </StyledPanesContainer>
+            )}
+          </PaneProvider>
+        </>
       ))}
     </div>
   );
