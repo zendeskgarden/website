753538b769ed1bb6213c18d0f8b39349ba878bb3
Jen Gorfine <jen.gorfine@zendesk.com>
2022-11-21 13:08:43 -0500

    feat(alerts): add type name to each Type demo's close button label

--- src/examples/notifications/alerts/AlertType.tsx
+++ src/examples/notifications/alerts/AlertType.tsx
@@ -18,25 +18,25 @@ const Example = () => (
     <Alert type="info">
       <Title>Info</Title>
       Turnip greens yarrow endive cauliflower sea lettuce kohlrabi amaranth water
-      <Close aria-label="Close Alert" />
+      <Close aria-label="Close Info Alert" />
     </Alert>
     <StyledSpacer />
     <Alert type="warning">
       <Title>Warning</Title>
       Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke
-      <Close aria-label="Close Alert" />
+      <Close aria-label="Close Warning Alert" />
     </Alert>
     <StyledSpacer />
     <Alert type="error">
       <Title>Error</Title>
       Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic
-      <Close aria-label="Close Alert" />
+      <Close aria-label="Close Error Alert" />
     </Alert>
     <StyledSpacer />
     <Alert type="success">
       <Title>Success</Title>
       Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke
-      <Close aria-label="Close Alert" />
+      <Close aria-label="Close Success Alert" />
     </Alert>
   </>
 );
