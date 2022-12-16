31bb7aeff128adb7b01d695a60fb9483944f1940
Mike Tobia <mike.tobia@zendesk.com>
2022-12-16 12:39:02 -0500

    chore(tables): update react components

--- src/examples/table/TableScroll.tsx
+++ src/examples/table/TableScroll.tsx
@@ -6,18 +6,7 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
-import { getColor } from '@zendeskgarden/react-theming';
 import { Body, Cell, Head, HeaderCell, HeaderRow, Row, Table } from '@zendeskgarden/react-tables';
-
-const StyledHeaderRow = styled(HeaderRow)`
-  position: sticky;
-  top: 0;
-  border-bottom-color: transparent;
-  box-shadow: inset 0 -${props => props.theme.borderWidths.sm} 0 ${props => getColor('neutralHue', 300, props.theme)};
-  background-color: ${props => props.theme.colors.background};
-`;
-
 interface IRow {
   index: number;
   fruit: string;
@@ -35,12 +24,12 @@ const rowData: IRow[] = Array.from(Array(100)).map((row, index) => ({
 const Example = () => (
   <div style={{ maxHeight: 500, overflowY: 'auto' }}>
     <Table style={{ minWidth: 500 }}>
-      <Head>
-        <StyledHeaderRow>
+      <Head isSticky>
+        <HeaderRow>
           <HeaderCell>Fruit</HeaderCell>
           <HeaderCell>Sun exposure</HeaderCell>
           <HeaderCell>Soil type</HeaderCell>
-        </StyledHeaderRow>
+        </HeaderRow>
       </Head>
       <Body>
         {rowData.map(data => (
