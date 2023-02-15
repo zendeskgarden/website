64a2a5706a032b17658024019f238fc3a53516e9
george treviranus <geowtrev@gmail.com>
2023-02-15 11:00:15 -0600

    refactor: code review

--- src/components/MarkdownProvider/components/PropSheet.tsx
+++ src/components/MarkdownProvider/components/PropSheet.tsx
@@ -102,7 +102,7 @@ export const PropSheet: React.FC<{
                       </MD>
                     </Cell>
                     <Cell>
-                      <MD tag="div" isMonospace={defaultMonospace}>
+                      <MD isMonospace={defaultMonospace}>
                         {prop.type === 'DefaultTheme' ? (
                           <Markdown>[DEFAULT_THEME](/components/theme-object)</Markdown>
                         ) : (
