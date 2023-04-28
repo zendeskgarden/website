66cd34eeea8f53e3389fb95b85cff971469a278a
george treviranus <geowtrev@gmail.com>
2023-04-28 11:22:03 -0500

    chore: more cleanup of dropzone default example

--- src/examples/draggable/DropzoneDefault.tsx
+++ src/examples/draggable/DropzoneDefault.tsx
@@ -7,12 +7,9 @@
 
 import React, { RefObject, forwardRef, useCallback, useEffect, useRef, useState } from 'react';
 import styled, { useTheme } from 'styled-components';
-
 import {
   Announcements,
-  UniqueIdentifier,
   MeasuringStrategy,
-  closestCorners,
   DndContext,
   useSensors,
   useSensor,
@@ -21,26 +18,12 @@ import {
   KeyboardSensor,
   KeyboardCoordinateGetter,
   DragOverlay,
-  CollisionDetection,
-  pointerWithin,
-  rectIntersection,
   DragStartEvent,
   DragOverEvent,
   DragEndEvent,
   useDraggable,
   useDroppable
 } from '@dnd-kit/core';
-import {
-  AnimateLayoutChanges,
-  SortableContext,
-  arrayMove,
-  defaultAnimateLayoutChanges,
-  sortableKeyboardCoordinates,
-  useSortable,
-  verticalListSortingStrategy
-} from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
-
 import {
   Draggable,
   DraggableList,
@@ -50,9 +33,9 @@ import {
 import { Row, Col } from '@zendeskgarden/react-grid';
 import { MD } from '@zendeskgarden/react-typography';
 
-type IColumns = Record<UniqueIdentifier, IDraggableItem[]>;
+type IColumns = Record<string, IColumnItem[]>;
 
-interface IDraggableItem {
+interface IColumnItem {
   id: string;
   value: string;
 }
@@ -64,15 +47,10 @@ interface IDraggableItemProps extends IDraggableProps {
   tabIndex?: number;
 }
 
-interface ISortableItemProps extends IDraggableItemProps {
-  showDropMessage: boolean;
-}
-
-interface ISortableColumnProps {
-  id: string;
-  items: IDraggableItem[];
-  activeId: UniqueIdentifier | null;
-  activeColumnId: UniqueIdentifier | null;
+interface IDroppableColumnProps {
+  items: IColumnItem[];
+  isActive: boolean;
+  isHighlighted: boolean;
 }
 
 const defaultColumns: IColumns = {
@@ -81,40 +59,10 @@ const defaultColumns: IColumns = {
     { id: '2', value: 'Orange' },
     { id: '3', value: 'Grapes' }
   ],
-  dropped: []
+  dropzone: []
 };
 
-const animateLayoutChanges: AnimateLayoutChanges = args =>
-  defaultAnimateLayoutChanges({ ...args, wasDragging: true });
-
-const getAnnouncements = (
-  getPosition: (id: UniqueIdentifier | undefined) => number,
-  count: number | undefined
-): Announcements => ({
-  onDragStart({ active }) {
-    return `Picked up sortable item ${active.id}. Sortable item ${
-      active.id
-    } is in position ${getPosition(active.id as UniqueIdentifier)} of ${count}`;
-  },
-  onDragOver({ active, over }) {
-    return `Sortable item ${active.id} was moved into position ${getPosition(
-      over?.id
-    )} of ${count}`;
-  },
-  onDragEnd({ active, over }) {
-    return `Sortable item ${active.id} was dropped at position ${getPosition(
-      over?.id
-    )} of ${count}`;
-  },
-  onDragCancel({ active }) {
-    return `Dragging was cancelled. Sortable item ${active.id} was dropped.`;
-  }
-});
-
-function findColumn(
-  id: UniqueIdentifier | undefined,
-  columns: IColumns
-): UniqueIdentifier | undefined {
+const getColumnId = (id: string | undefined | null, columns: IColumns): string | undefined => {
   if (!id) return undefined;
 
   if (id in columns) {
@@ -124,22 +72,12 @@ function findColumn(
   return Object.keys(columns).find(cId => {
     return columns[cId].findIndex(item => item?.id === id) > -1;
   });
-}
+};
 
 const coordinateGetter: KeyboardCoordinateGetter = (event, args) => {
   const { context, currentCoordinates } = args;
   const { active, droppableRects } = context;
   const isDraggableList = active?.data?.current?.type === 'draggable';
-
-  const isOrphanItem = active?.data.current?.sortable?.items.length === 1;
-  const hasNoSortables = active?.data.current?.sortable?.items.length === 0;
-
-  // Prevent orphan sortable item from bouncing between outer and inner droppable areas.
-  if (isOrphanItem || hasNoSortables) {
-    event.preventDefault();
-
-    return undefined;
-  }
 
   if (isDraggableList && ['ArrowDown', 'ArrowUp', 'ArrowRight', 'ArrowLeft'].includes(event.key)) {
     event.preventDefault();
@@ -161,18 +99,27 @@ const coordinateGetter: KeyboardCoordinateGetter = (event, args) => {
     }
   }
 
-  return sortableKeyboardCoordinates(event, args);
+  return undefined;
 };
 
-const collisionDetection: CollisionDetection = args => {
-  const collisions = [...pointerWithin(args), ...rectIntersection(args)];
+const getAnnouncements = (count: number): Announcements => ({
+  onDragStart({ active }) {
+    return `Picked up draggable item ${active.id}.`;
+  },
+  onDragOver({ active, over }) {
+    if (over?.id) {
+      return `Draggable item ${active.id} is over droppable area ${over.id}.`;
+    }
 
-  if (collisions.length > 0) {
-    return closestCorners(args);
+    return undefined;
+  },
+  onDragEnd({ active }) {
+    return `Draggable item ${active.id} added to dropzone. Dropzone has ${count} items.`;
+  },
+  onDragCancel({ active }) {
+    return `Dragging was cancelled. Draggable item ${active.id} was dropped`;
   }
-
-  return collisions;
-};
+});
 
 const StyledHeading = styled(MD)`
   margin-bottom: ${p => p.theme.space.xs};
@@ -227,75 +174,45 @@ const DraggableListItem = ({ id, value }: IDraggableItemProps) => {
   );
 };
 
-const SortableListItem = ({ id, value, showDropMessage }: ISortableItemProps) => {
-  const { active, attributes, listeners, setNodeRef, setActivatorNodeRef, transform, transition } =
-    useSortable({
-      id: id!,
-      animateLayoutChanges
-    });
+const DroppableColumn = ({ items, isActive, isHighlighted }: IDroppableColumnProps) => {
+  const { setNodeRef } = useDroppable({ id: 'dropzone' });
+  const isEmpty = items.length === 0;
+  let message;
 
-  const isActiveItem = active?.id === id;
-  const transformValue = CSS.Transform.toString(transform);
-  const style: React.CSSProperties = {
-    transition,
-    transform: transformValue,
-    opacity: isActiveItem ? 0 : 1
-  };
+  if (isEmpty && isHighlighted) {
+    message = 'Drop item here';
+  } else if (isEmpty) {
+    message = 'Drag to add';
+  }
 
   return (
-    <DraggableList.Item ref={setNodeRef} style={style}>
-      <DraggableItem
-        value={value}
-        {...attributes}
-        {...listeners}
-        style={{ display: showDropMessage ? 'none' : 'flex' }}
-        ref={setActivatorNodeRef}
-      />
-    </DraggableList.Item>
-  );
-};
-
-const SortableColumn = ({ id, items, activeId, activeColumnId }: ISortableColumnProps) => {
-  const { setNodeRef } = useDroppable({ id });
-  const isActive = !!activeId;
-  const isHighlighted = activeColumnId === id;
-  const showDropMessage = items.length === 1 && isHighlighted;
-
-  return (
-    <>
-      <StyledHeading isBold>Favorite fruits</StyledHeading>
-      <StyledDropzone ref={setNodeRef} isActive={isActive} isHighlighted={isHighlighted}>
-        <SortableContext id={id} items={items} strategy={verticalListSortingStrategy}>
-          <DraggableList>
-            {items.map(item => (
-              <SortableListItem {...item} key={item.id} showDropMessage={showDropMessage} />
-            ))}
-          </DraggableList>
-          {items.length === 0 && <Dropzone.Message>Drag to add</Dropzone.Message>}
-          {showDropMessage && <Dropzone.Message>Drop item here</Dropzone.Message>}
-        </SortableContext>
-      </StyledDropzone>
-    </>
+    <StyledDropzone ref={setNodeRef} isActive={isActive} isHighlighted={isHighlighted}>
+      {!isEmpty && (
+        <DraggableList>
+          {items.map(item => (
+            <DraggableList.Item key={item.id}>
+              <DraggableItem {...item} />
+            </DraggableList.Item>
+          ))}
+        </DraggableList>
+      )}
+      {message && <Dropzone.Message>{message}</Dropzone.Message>}
+    </StyledDropzone>
   );
 };
 
 const Example = () => {
   const [columns, setColumns] = useState<IColumns>(defaultColumns);
-  const [snapshot, setSnapshot] = useState<IColumns | null>(null);
-  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
-  const [startColumnId, setStartColumnId] = useState<UniqueIdentifier | null>(null);
-  const [overlayWidth, setOverlayWidth] = useState<number | undefined>(undefined);
+  const [activeId, setActiveId] = useState<string | null>(null);
+  const [dropzoneIsHighlighted, setDropzoneIsHighlighted] = useState<boolean>(false);
 
   const theme = useTheme();
   const overlayRef = useRef<HTMLDivElement>(null);
 
-  const activeColumnId = activeId ? (findColumn(activeId, columns) as UniqueIdentifier) : null;
-  const activeItem = columns[activeColumnId as UniqueIdentifier]?.find(
-    item => item.id === activeId
-  );
-
-  const draggablesColId = Object.keys(columns)[0];
-  const sortablesColId = Object.keys(columns)[1];
+  const activeItem = columns.draggables.find(item => item.id === activeId);
+  const draggableItems = columns.draggables;
+  const dropzoneItems = columns.dropzone;
+  const dropzoneItemCount = dropzoneItems.length;
 
   const sensors = useSensors(
     useSensor(MouseSensor),
@@ -303,77 +220,24 @@ const Example = () => {
     useSensor(KeyboardSensor, { coordinateGetter })
   );
 
-  const onDragStart = ({ active }: DragStartEvent) => {
-    const columnId = findColumn(active.id, columns) as UniqueIdentifier;
+  const onDragStart = useCallback(({ active }: DragStartEvent) => {
+    setActiveId(active.id as string);
+  }, []);
 
-    setActiveId(active.id);
-    setStartColumnId(columnId);
-    setSnapshot(columns);
-  };
+  const onDragOver = useCallback(({ over }: DragOverEvent) => {
+    const overId = over?.id;
 
-  const onDragOver = useCallback(
-    ({ active, over }: DragOverEvent) => {
-      const overId = over?.id;
+    if (!overId) {
+      setDropzoneIsHighlighted(false);
 
-      if (startColumnId === draggablesColId && !overId) {
-        setColumns(snapshot!);
-        setOverlayWidth(undefined);
+      return;
+    }
 
-        return;
-      }
-
-      if (!overId || active.id in columns) return;
-
-      if (overId !== 'dropped' && over?.rect.width && overlayWidth !== over.rect.width) {
-        setOverlayWidth(over.rect.width);
-      }
-
-      const overColId = findColumn(overId, columns);
-      const activeColId = findColumn(active.id, columns);
-
-      if (!overColId || !activeColId) return;
-
-      setColumns(prevColumns => {
-        const nextColumns = { ...prevColumns };
-        const activeItems = nextColumns[activeColId];
-        const overItems = nextColumns[overColId];
-        const activeIndex = activeItems.findIndex(item => item.id === active.id);
-        const overIndex = overItems.findIndex(item => item.id === overId);
-        let newIndex: number;
-
-        if (overId in nextColumns) {
-          const length = overItems.length;
-
-          newIndex = length > 0 ? length + 1 : length;
-        } else {
-          const isBelowLastItem =
-            over &&
-            overIndex === overItems.length - 1 &&
-            active.rect?.current?.translated?.top &&
-            active.rect.current.translated.top > over.rect.top + over.rect.height;
-
-          const modifier = isBelowLastItem ? 1 : 0;
-
-          newIndex = overIndex >= 0 ? overIndex + modifier : overItems.length + 1;
-        }
-
-        nextColumns[activeColId] = nextColumns[activeColId].filter(item => item.id !== active.id);
-
-        nextColumns[overColId] = [
-          ...nextColumns[overColId].slice(0, newIndex),
-          prevColumns[activeColId][activeIndex],
-          ...nextColumns[overColId].slice(newIndex, nextColumns[overColId].length)
-        ];
-
-        return nextColumns;
-      });
-    },
-    [columns, snapshot, overlayWidth, draggablesColId, startColumnId]
-  );
+    setDropzoneIsHighlighted((overId as string) === 'dropzone');
+  }, []);
 
   const onDragEnd = useCallback(
     ({ active, over }: DragEndEvent) => {
-      const activeColId = findColumn(active.id, columns) as UniqueIdentifier;
       const overId = over?.id;
 
       if (!overId) {
@@ -382,54 +246,32 @@ const Example = () => {
         return;
       }
 
-      const overColId = findColumn(overId, columns);
+      const activeColId = getColumnId(active.id as string, columns);
+      const overColId = getColumnId(overId as string, columns);
 
-      if (!overColId) return;
+      if (!overColId || !activeColId) return;
 
-      const activeIndex = columns[activeColId].findIndex(item => item.id === active.id);
-      const overIndex = columns[overColId].findIndex(item => item.id === overId);
-
-      if (activeIndex !== overIndex) {
-        setColumns(prevColumns => {
-          const nextColumns = { ...prevColumns };
-
-          nextColumns[overColId] = arrayMove(prevColumns[overColId], activeIndex, overIndex);
-
-          return nextColumns;
+      if (overId in columns) {
+        setColumns({
+          [activeColId]: columns[activeColId].filter(item => item.id !== active.id),
+          [overColId]: [...columns[overColId], activeItem!]
         });
       }
 
-      setSnapshot(null);
       setActiveId(null);
-      setOverlayWidth(undefined);
+      setDropzoneIsHighlighted(false);
     },
-    [columns]
+    [columns, activeItem]
   );
 
-  const onDragCancel = () => {
-    setColumns(snapshot!);
-    setSnapshot(null);
+  const onDragCancel = useCallback(() => {
     setActiveId(null);
-    setOverlayWidth(undefined);
-  };
-
-  const getPosition = (id: UniqueIdentifier | undefined) => {
-    if (!id) return 1;
-
-    const activeColId = findColumn(id, columns) as string;
-    const itemIndex = columns[activeColId]?.findIndex(item => item.id === id) || 0;
-
-    return itemIndex + 1;
-  };
-
-  const activeItemsCount = columns[activeColumnId!]?.length;
-  const draggables = columns[draggablesColId];
-  const sortables = columns[sortablesColId];
+    setDropzoneIsHighlighted(false);
+  }, []);
 
   return (
     <DndContext
-      accessibility={{ announcements: getAnnouncements(getPosition, activeItemsCount) }}
-      collisionDetection={collisionDetection}
+      accessibility={{ announcements: getAnnouncements(dropzoneItemCount) }}
       sensors={sensors}
       onDragStart={onDragStart}
       onDragOver={onDragOver}
@@ -441,22 +283,22 @@ const Example = () => {
         <Col size={4}>
           <StyledHeading isBold>List of produce</StyledHeading>
           <DraggableList>
-            {draggables.map(item => (
+            {draggableItems.map(item => (
               <DraggableListItem key={item.id} {...item} />
             ))}
           </DraggableList>
         </Col>
         <Col size={5}>
-          <SortableColumn
-            id={sortablesColId}
-            items={sortables}
-            activeId={activeId}
-            activeColumnId={activeColumnId}
+          <StyledHeading isBold>Favorite fruits</StyledHeading>
+          <DroppableColumn
+            items={dropzoneItems}
+            isActive={!!activeId}
+            isHighlighted={dropzoneIsHighlighted}
           />
         </Col>
         <DragOverlay>
           {activeItem && (
-            <div style={{ width: overlayWidth, padding: `${theme.space.xxs} 0` }}>
+            <div style={{ padding: `${theme.space.xxs} 0` }}>
               <DraggableItem
                 ref={overlayRef}
                 id={activeItem.id}
