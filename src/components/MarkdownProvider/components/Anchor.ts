453488e5fb2bc8c367f58481d0c92ef3102caf39
Mike Tobia <mike.tobia@zendesk.com>
2023-06-22 15:25:58 -0400

    fix: address feedback for anchors

--- src/components/MarkdownProvider/components/Anchor.ts
+++ src/components/MarkdownProvider/components/Anchor.ts
@@ -6,7 +6,7 @@
  */
 
 import styled from 'styled-components';
-import { SELECTOR_FOCUS_VISIBLE, mediaQuery } from '@zendeskgarden/react-theming';
+import { SELECTOR_FOCUS_VISIBLE, focusStyles, mediaQuery } from '@zendeskgarden/react-theming';
 import { Anchor } from '@zendeskgarden/react-buttons';
 import { math } from 'polished';
 
@@ -14,18 +14,23 @@ export const StyledAnchor = styled(Anchor).attrs(props => ({
   /* eslint-disable-next-line prefer-named-capture-group */
   isExternal: props.href && !/^(#|\/(?!\/))/u.test(props.href)
 }))`
+  ${({ theme }) =>
+    focusStyles({
+      theme,
+      inset: true,
+      styles: { outlineColor: 'transparent' }
+    })}
+
   &.anchor.before {
     position: relative;
-    margin-right: ${p => p.theme.space.base}px;
-    margin-left: -${p => math(`${p.theme.space.xs} * 2 + ${p.theme.iconSizes.md} + ${p.theme.space.base}`)};
+    margin-left: -${p => math(`${p.theme.space.xs} * 2 + ${p.theme.iconSizes.md}`)};
     border-radius: ${p => p.theme.borderRadii.md};
     padding: 0 ${p => p.theme.iconSizes.md};
     color: transparent;
 
     /* stylelint-disable selector-max-specificity */
     ${p => mediaQuery('down', 'md', p.theme)} {
-      margin-right: ${p => p.theme.space.base}px;
-      margin-left: -${p => math(`${p.theme.space.xxs} * 2 + ${p.theme.iconSizes.md} + ${p.theme.space.base}`)};
+      margin-left: -${p => math(`${p.theme.space.xxs} * 2 + ${p.theme.iconSizes.md}`)};
       padding: 0 ${p => p.theme.space.sm};
     }
 
