8e38475cd5c3a21901592810f4f0ab1bcafa4892
George Treviranus <3946669+geotrev@users.noreply.github.com>
2024-08-13 10:45:24 -0500

    feat(components): update draggable to v9 (#619)

--- src/examples/draggable/DraggableListIndicator.tsx
+++ src/examples/draggable/DraggableListIndicator.tsx
@@ -6,9 +6,10 @@
  */
 
 import React from 'react';
-import { Row, Col } from '@zendeskgarden/react-grid';
+import { getColor } from '@zendeskgarden/react-theming';
+import { Grid } from '@zendeskgarden/react-grid';
 import { MD } from '@zendeskgarden/react-typography';
-import { Draggable, DraggableList } from '@zendeskgarden/react-drag-drop';
+import { Draggable, DraggableList } from '@zendeskgarden/react-draggable';
 import styled from 'styled-components';
 
 const items = [
@@ -21,11 +22,12 @@ const items = [
 
 const StyledHeading = styled(MD)`
   margin-bottom: ${p => p.theme.space.xs};
+  color: ${p => getColor({ variable: 'foreground.default', theme: p.theme })};
 `;
 
 const Example = () => (
-  <Row justifyContent="center">
-    <Col sm={4}>
+  <Grid.Row justifyContent="center">
+    <Grid.Col sm={4}>
       <StyledHeading isBold tag="h4">
         Favorites
       </StyledHeading>
@@ -46,8 +48,8 @@ const Example = () => (
           )
         )}
       </DraggableList>
-    </Col>
-  </Row>
+    </Grid.Col>
+  </Grid.Row>
 );
 
 export default Example;
