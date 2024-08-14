c5329e737b88413c2de126956c06b06eac80b968
Florent <ze-flo@users.noreply.github.com>
2024-08-14 09:36:24 -0700

    feat(components): update `tabs` to v9 (#626)

--- src/examples/tabs/TabsDisabled.tsx
+++ src/examples/tabs/TabsDisabled.tsx
@@ -6,36 +6,36 @@
  */
 
 import React, { useState } from 'react';
-import { Tabs, TabList, Tab, TabPanel } from '@zendeskgarden/react-tabs';
+import { Tabs } from '@zendeskgarden/react-tabs';
 
 const Example = () => {
   const [selectedTab, setSelectedTab] = useState('tab-1');
 
   return (
     <Tabs selectedItem={selectedTab} onChange={setSelectedTab}>
-      <TabList>
-        <Tab item="tab-1">Elm</Tab>
-        <Tab item="tab-2" disabled>
+      <Tabs.TabList>
+        <Tabs.Tab item="tab-1">Elm</Tabs.Tab>
+        <Tabs.Tab item="tab-2" disabled>
           Sugar Maple
-        </Tab>
-        <Tab item="tab-3" disabled>
+        </Tabs.Tab>
+        <Tabs.Tab item="tab-3" disabled>
           Dogwood
-        </Tab>
-      </TabList>
-      <TabPanel item="tab-1">
+        </Tabs.Tab>
+      </Tabs.TabList>
+      <Tabs.TabPanel item="tab-1">
         Elms are deciduous and semi-deciduous trees comprising the flowering plant genus Ulmus in
         the plant family Ulmaceae.
-      </TabPanel>
-      <TabPanel item="tab-2">
+      </Tabs.TabPanel>
+      <Tabs.TabPanel item="tab-2">
         The sugar maple is one of America’s most-loved trees. In fact, more states have claimed it
         as their state tree than any other single species—for New York, West Virginia, Wisconsin,
         and Vermont, the maple tree stands alone.
-      </TabPanel>
-      <TabPanel item="tab-3">
+      </Tabs.TabPanel>
+      <Tabs.TabPanel item="tab-3">
         Cornus is a genus of about 30–60 species of woody plants in the family Cornaceae, commonly
         known as dogwoods, which can generally be distinguished by their blossoms, berries, and
         distinctive bark.
-      </TabPanel>
+      </Tabs.TabPanel>
     </Tabs>
   );
 };
