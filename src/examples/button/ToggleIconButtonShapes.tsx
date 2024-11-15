5fe9c1032f04c9f4aecfc881219408f4701a908e
Florent <ze-flo@users.noreply.github.com>
2024-11-15 15:06:59 -0800

    docs: remove discouraged `isBare` `Input` ex + fix insufficient contrast in `Toggle Icon Button` examples (#670)

--- src/examples/button/ToggleIconButtonShapes.tsx
+++ src/examples/button/ToggleIconButtonShapes.tsx
@@ -8,8 +8,10 @@
 import React, { useState } from 'react';
 import { Grid } from '@zendeskgarden/react-grid';
 import { ToggleIconButton } from '@zendeskgarden/react-buttons';
-import { ReactComponent as RoundIcon } from '@zendeskgarden/svg-icons/src/16/leaf-stroke.svg';
-import { ReactComponent as SquareIcon } from '@zendeskgarden/svg-icons/src/16/bold-stroke.svg';
+import { ReactComponent as RoundStrokeIcon } from '@zendeskgarden/svg-icons/src/16/leaf-stroke.svg';
+import { ReactComponent as RoundFillIcon } from '@zendeskgarden/svg-icons/src/16/leaf-fill.svg';
+import { ReactComponent as SquareStrokeIcon } from '@zendeskgarden/svg-icons/src/16/bold-stroke.svg';
+import { ReactComponent as SquareFillIcon } from '@zendeskgarden/svg-icons/src/16/bold-fill.svg';
 import { Tooltip } from '@zendeskgarden/react-tooltips';
 
 const Example = () => {
@@ -28,7 +30,10 @@ const Example = () => {
               setRoundPressed(!roundPressed);
             }}
           >
-            <RoundIcon />
+            <svg focusable="false">
+              <RoundFillIcon style={{ opacity: roundPressed ? 1 : 0 }} />
+              <RoundStrokeIcon style={{ opacity: roundPressed ? 0 : 1 }} />
+            </svg>
           </ToggleIconButton>
         </Tooltip>
       </Grid.Col>
@@ -43,7 +48,10 @@ const Example = () => {
               setSquarePressed(!squarePressed);
             }}
           >
-            <SquareIcon />
+            <svg focusable="false">
+              <SquareFillIcon style={{ opacity: squarePressed ? 1 : 0 }} />
+              <SquareStrokeIcon style={{ opacity: squarePressed ? 0 : 1 }} />
+            </svg>
           </ToggleIconButton>
         </Tooltip>
       </Grid.Col>
